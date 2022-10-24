c5c168f997e802365cc93bad49ee58ab9722c435
Ovee Hasnat <abu.hasnat@northsouth.edu>
2022-10-24 15:26:36 +0600

    Body Contents Added

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Menu from "./menu";
+import Home from "./home";
+import Contact from "./contact";
+import About from "./about";
 
 const Body = () => {
     return (
